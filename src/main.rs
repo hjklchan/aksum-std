38282e9eac9e9afda906eedaf5518997a45fce1b
Illusion <1161524405@qq.com>
2024-05-15 18:54:30 +0800

    add comments

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 use std::net::SocketAddr;
 
 use axum::Router;
+use sqlx::{mysql::MySqlPoolOptions, MySql, Pool};
 use tokio::net::TcpListener;
 
 mod routes;
 
+#[derive(Clone)]
+pub struct AppState {
+    db: Pool<MySql>,
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().nest("/tickets", routes::tickets_router());
+    // initialize database connect pool
+    let db = MySqlPoolOptions::new()
+        .connect("mysql://root:@127.0.0.1:3306/aksum_std")
+        .await
+        .unwrap();
 
+    // initialize state and app instance
+    let app_state = AppState { db };
+    let app = Router::new()
+        .with_state(app_state)
+        .nest("/tickets", routes::tickets_router());
+
+    // create a tcp listener
     let addr = SocketAddr::from(([127, 0, 0, 1], 8899));
     let tcp_listener = TcpListener::bind(addr).await.unwrap();
     println!("Listening on http://{}", addr);
 
+    // run http server
     axum::serve(tcp_listener, app).await.unwrap();
 }
