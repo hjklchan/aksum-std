c479ae0e292f2abb7fa3d1a8f843a76a282692a3
Illusion <1161524405@qq.com>
2024-05-15 20:10:06 +0800

    add column `email`

--- migrations/20240515120400_create_users_table.up.sql
+++ migrations/20240515120400_create_users_table.up.sql
@@ -1,11 +1,12 @@
 -- Add up migration script here
 CREATE TABLE `users` (
   `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `avatar_url` varchar(255) DEFAULT NULL,
-  `phone_number` varchar(11) DEFAULT NULL,
+  `username` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `avatar_url` VARCHAR(255) DEFAULT NULL,
+  `phone_number` VARCHAR(11) DEFAULT NULL,
   `status` TINYINT NOT NULL DEFAULT 0,
-  `password` varchar(255) DEFAULT NULL,
+  `password` VARCHAR(255) DEFAULT NULL,
   `created_at` TIMESTAMP NULL DEFAULT NULL,
   `updated_at` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
