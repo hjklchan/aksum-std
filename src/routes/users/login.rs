278d53019793657fc69de1cd48c89ebb8fd57cce
Illusion <1161524405@qq.com>
2024-05-16 23:15:58 +0800

    basic logic

--- src/routes/users/login.rs
+++ src/routes/users/login.rs
@@ -1,7 +1,7 @@
+use crate::errors::Error;
 use crate::AppState;
-use axum::{extract::State, response::IntoResponse, Json};
+use axum::{extract::State, Json};
 use serde::{Deserialize, Serialize};
-// use sqlx::{MySql, Pool};
 
 #[derive(Debug, Deserialize)]
 pub struct LoginReq {
@@ -18,16 +18,41 @@ pub struct LoginRep {
 pub async fn login_handler(
     State(state): State<AppState>,
     Json(payload): Json<LoginReq>,
-) -> impl IntoResponse {
+) -> Result<Json<LoginRep>, Error> {
     println!("email: {}\tpassword: {}", payload.email, payload.password);
     // if the user was exist, direct to login and gen token
-    let _result = sqlx::query!(
+    let result = sqlx::query!(
         r#"SELECT EXISTS ( SELECT 1 FROM `users` WHERE `email` = ? ) AS `exists!`"#,
         &payload.email
     )
     .fetch_one(&state.db)
-    .await;
+    .await
+    .map_err(|err| Error::Sqlxxx(err))?;
+
+    // user not found
+    if result.exists != 1 {
+        return Err(Error::not_found("user"));
+    }
+
+    // check if the password is correct
+    // TODO: to prepare sql
+    let result = sqlx::query!(
+        r#"SELECT EXISTS ( SELECT 1 FROM `users` WHERE `email` = ? AND `password` = ? ) AS `exists!`"#,
+        &payload.email,
+        &payload.password
+    )
+        .fetch_one(&state.db)
+        .await
+        .map_err(|err| Error::Sqlxxx(err))?;
+    if result.exists != 1 {
+        return Err(Error::BadRequest("the user's password is incorrect".into()));
+    }
+
+    // TODO: Generate token here
 
     // Response to be designed
-    ()
+    Ok(Json(LoginRep {
+        access_token: "".into(),
+        token_type: "Bearer".into(),
+    }))
 }
