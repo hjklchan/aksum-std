87073a7af62c212888161044852b1321e036906a
Lucas chen <lucas.chen@gbo-asia.com>
2024-05-16 14:17:00 +0800

    ordinary implementation for login handler

--- src/routes/users/login.rs
+++ src/routes/users/login.rs
@@ -1,10 +1,14 @@
-use axum::{http::StatusCode, Json};
+use std::sync::Arc;
+
+use crate::{models::UserModel, AppState};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
+use sqlx::{MySql, Pool};
 
 #[derive(Debug, Deserialize)]
 pub struct LoginReq {
     email: String,
-    password: String
+    password: String,
 }
 
 #[derive(Debug, Serialize)]
@@ -13,6 +17,19 @@ pub struct LoginRep {
     token_type: String,
 }
 
-pub async fn login_handler() -> Result<Json<LoginRep>, StatusCode> {
-    todo!()
+pub async fn login_handler(
+    State(state): State<AppState>,
+    Json(payload): Json<LoginReq>,
+) -> impl IntoResponse {
+    println!("email: {}\tpassword: {}", payload.email, payload.password);
+    // if the user was exist, direct to login and gen token
+    let _result = sqlx::query!(
+        r#"SELECT EXISTS ( SELECT 1 FROM `users` WHERE `email` = ? ) AS `exists!`"#,
+        &payload.email
+    )
+    .fetch_one(&state.db)
+    .await;
+
+    // Response to be designed
+    ()
 }
